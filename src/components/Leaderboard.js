563eb62d967098444ade1c9682208f6cf5ddaffe
mehulagr <agrawal.mehul92@gmail.com>
2020-04-21 13:20:18 -0700

    It is still working with protected route

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,54 +1,47 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Route, Redirect } from "react-router-dom";
 
 class Leaderboard extends Component {
   render() {
-    const {users, ids, authedUser} = this.props
+    const {users, ids} = this.props
     
     return (
-      <Route>
-        {authedUser.id ? 
-          <div className='container'>
-              <ul className='dashboard-list'>
-                {users && ids.map((id) => (
-                  <li key={id} className="mb-2">
-                      <div className="card flex-row flex-wrap">
-                        <div className="card-block p-2 flex-center">
-                            <img className="rounded-circle avatar-image" src={users[id].avatarURL} alt={users[id].name}></img>
-                        </div>
-                        <div className="card-block px-4 py-2 card-content-2">
-                            <h4 className="card-title">{users[id].name}</h4>
-                            <p className="card-text">Number of answers: {Object.keys(users[id].answers).length}</p>
-                            <p className="card-text">Number of questions: {users[id].questions.length}</p>
-                        </div>
-                        <div className="card-block p-2 align-middle">
-                            <p className="text-center mb-1">Score</p>
-                            <div className="circle">
-                              <p>{Object.keys(users[id].answers).length + users[id].questions.length}</p> 
-                            </div>
+      <div className='container'>
+          <ul className='dashboard-list'>
+            {users && ids.map((id) => (
+              <li key={id} className="mb-2">
+                  <div className="card flex-row flex-wrap">
+                    <div className="card-block p-2 flex-center">
+                        <img className="rounded-circle avatar-image" src={users[id].avatarURL} alt={users[id].name}></img>
+                    </div>
+                    <div className="card-block px-4 py-2 card-content-2">
+                        <h4 className="card-title">{users[id].name}</h4>
+                        <p className="card-text">Number of answers: {Object.keys(users[id].answers).length}</p>
+                        <p className="card-text">Number of questions: {users[id].questions.length}</p>
+                    </div>
+                    <div className="card-block p-2 align-middle">
+                        <p className="text-center mb-1">Score</p>
+                        <div className="circle">
+                          <p>{Object.keys(users[id].answers).length + users[id].questions.length}</p> 
                         </div>
                     </div>
-                  </li>
-                ))}
-              </ul>
-          </div>
-          : <Redirect to="/" />
-        }
-      </Route>
+                </div>
+              </li>
+            ))}
+          </ul>
+      </div>
     )
   }
 }
 
-function mapStateToProps({users, authedUser}) {
+function mapStateToProps({users}) {
   const userScore = user => {
     return Object.keys(users[user].answers).length + users[user].questions.length
   }
   return {
     users,
     ids: Object.keys(users)
-          .sort((a, b) => userScore(b) - userScore(a)),
-    authedUser
+          .sort((a, b) => userScore(b) - userScore(a))
   }
 }
 
--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -6,32 +6,38 @@ import AnswerQuestion from './AnswerQuestion';
 import NewQuestion from './NewQuestion';
 import Login from './Login';
 import GameNav from './Nav';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import NotFound from './NotFound';
 
 class LoggedIn extends Component {
     render() {
         const {authedUser} = this.props;
+
+        const PrivateRoute = ({ component: Component, ...rest }) => (
+            <Route {...rest} render={(props) => (
+              authedUser.id
+                ? <Component {...props} />
+                : <Redirect to={{
+                    pathname: '/',
+                    state: { from: props.location }
+                  }} />
+            )} />
+        )
       
         return (
-            <div>
-                {   this.props.authedUser.id 
-                    ?   <Route path='/' exact component={Login}/> 
-                    :   <Fragment>
-                            <GameNav authedUser={authedUser}/>
-                            <div className="container">
-                                <Switch>
-                                    <PrivateRoute path='/dashboard' exact component={QuestionsListPage} />
-                                    <PrivateRoute path='/question/:id' component={AnswerQuestion} />
-                                    <PrivateRoute path='/add' component={NewQuestion} />
-                                    <PrivateRoute path='/leaderboard' component={Leaderboard} />
-                                    <Route exact path='/' component={Login} />
-                                    <Route component={NotFound} />
-                                </Switch>
-                            </div>
-                        </Fragment>
-                }
-            </div>
+            <Router>
+                <GameNav authedUser={authedUser}/>
+                <div className="container">
+                    <Switch>
+                    <PrivateRoute path='/dashboard' exact component={QuestionsListPage} />
+                    <PrivateRoute path='/question/:id' component={AnswerQuestion} />
+                    <PrivateRoute path='/add' component={NewQuestion} />
+                    <PrivateRoute path='/leaderboard' component={Leaderboard} />
+                    <Route exact path='/' component={Login} />
+                    <Route component={NotFound} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
   }
